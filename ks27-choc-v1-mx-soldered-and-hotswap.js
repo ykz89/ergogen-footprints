01c8cd03ff9bd66b283f82c7059b11b7be8ed26f
Ying Kun Zhan <yingkun@zhan.co.nl>
2024-10-31 01:29:34 +0100

    Revert last commit

--- ks27-choc-v1-mx-soldered-and-hotswap.js
+++ ks27-choc-v1-mx-soldered-and-hotswap.js
@@ -13,7 +13,7 @@ module.exports = {
             ${p.at /* parametric position */}
             (layer "${p.side}.Cu")
             ${'' /* footprint reference */}
-            (property Reference "${p.ref}" 
+            (property reference "${p.ref}" 
                 (at 0.1 -8.5 ${p.r}) 
                 (layer ${p.side}.SilkS) 
                 ${p.ref_hide}
